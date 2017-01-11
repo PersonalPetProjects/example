1006ad89badfe2795bc5b40ac055ab257dfde275
PersonalPetProjects <nicholas.pruden22@gmail.com>
2017-01-11 12:09:29 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+			System.out.print("im a little nervous.");
+
+			
+			int result = newClass();
+			System.out.println(result);
+	}
+
+	public static int newClass() {
+		
+		int four = 4;
+		int five = 5;
+		int that = 0;
+		that = four * five;
+		return that;
+	}
+}
+ 
